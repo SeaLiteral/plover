b0d67575b529b435059cea80b8793594090c8005
SeaLiteral <34283904+SeaLiteral@users.noreply.github.com>
2018-08-13 20:58:20 +0200

    Extend to work for both pre- and suffixes

--- plover/orthography.py
+++ plover/orthography.py
@@ -6,32 +6,41 @@
 from plover import system
 
 
-def make_candidates_from_rules(word, suffix, check=lambda x: True):
+def make_candidates_from_rules(word, affix, is_prefix, check=lambda x: True):
     candidates = []
-    for r in system.ORTHOGRAPHY_RULES:
-        m = r[0].match(word + " ^ " + suffix)
+    rules = system.ORTHOGRAPHY_RULES
+    if is_prefix:
+        rules = system.PREFIX_ORTHOGRAPHY_RULES
+
+    for r in rules:
+        if is_prefix:
+            m = r[0].match(affix + " ^ " + word)
+        else:
+            m = r[0].match(word + " ^ " + affix)
         if m:   
             expanded = m.expand(r[1])
             if check(expanded):
                 candidates.append(expanded)
     return candidates
 
-def _add_suffix(word, suffix):
+def _add_affix(word, affix, is_prefix):
     in_dict_f = lambda x: x in system.ORTHOGRAPHY_WORDS
 
     candidates = []
     
-    alias = system.ORTHOGRAPHY_RULES_ALIASES.get(suffix, None)
+    alias = system.ORTHOGRAPHY_RULES_ALIASES.get(affix, None)
     if alias is not None:
-        candidates.extend(make_candidates_from_rules(word, alias, in_dict_f))
+        candidates.extend(make_candidates_from_rules(word, alias, is_prefix, in_dict_f))
     
     # Try a simple join if it is in the dictionary.
-    simple = word + suffix
+    simple = word + affix
+    if is_prefix:
+        simple = affix + word
     if in_dict_f(simple):
         candidates.append(simple)
     
     # Try rules with dict lookup.
-    candidates.extend(make_candidates_from_rules(word, suffix, in_dict_f))
+    candidates.extend(make_candidates_from_rules(word, affix, is_prefix, in_dict_f))
 
     # For all candidates sort by prominence in dictionary and, since sort is
     # stable, also by the order added to candidates list.
@@ -40,7 +49,7 @@ def _add_suffix(word, suffix):
         return candidates[0]
     
     # Try rules without dict lookup.
-    candidates = make_candidates_from_rules(word, suffix)
+    candidates = make_candidates_from_rules(word, affix, is_prefix)
     if candidates:
         return candidates[0]
     
@@ -57,5 +66,18 @@ def add_suffix(word, suffix):
     
     """
     suffix, sep, rest = suffix.partition(' ')
-    expanded = _add_suffix(word, suffix)
+    expanded = _add_affix(word, suffix, False)
     return expanded + sep + rest
+
+def add_prefix(word, prefix):
+    """Add a prefix to a word by applying the rules above
+    
+    Arguments:
+        
+    word -- A word
+    prefix -- The prefix to add
+    
+    """
+    rest, sep, prefix = prefix.rpartition(' ')
+    expanded = _add_affix(word, prefix, True)
+    return rest + sep + expanded
